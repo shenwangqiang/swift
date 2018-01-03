e09fb544485b22bcbd65f17ed6072b9f2f206980
shenwangqiang <shenwangqang88@163.com>
2018-01-03 12:29:08 +0800

    界面搭建

---
+++ contact/contact/ListTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListTableViewController.swift
+//  contact
+//
+//  Created by 沈王强 on 2018/1/3.
+//  Copyright © 2018年 沈王强. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+  
+}
