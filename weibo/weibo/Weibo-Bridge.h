2a7fce471e6954b246bb3bcc1543da62b08f184b
shenwangqiang <shenwangqang88@163.com>
2018-01-05 18:05:33 +0800

    添加桥接文件，swift调用oc

---
+++ weibo/weibo/Weibo-Bridge.h
@@ -0,0 +1,10 @@
+//
+//  Weibo-Bridge.h
+//  weibo
+//
+//  Created by 沈王强 on 2018/1/5.
+//  Copyright © 2018年 沈王强. All rights reserved.
+//
+
+// 桥接文件，专门用于引入 OC 的头文件，一旦引入，Swift 就可以正常使用(宏除外)
+#import "CZAdditions.h"
